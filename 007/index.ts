6486c8e2a4af81cb97ae612e195c07d1ae76bee9
msejak <7o.mseJak@gmail.com>
2023-12-25 12:25:00 +0300

    update: leesson 007

---
+++ 007/index.ts
@@ -0,0 +1,11 @@
+// let userName: string = 'Ivan';
+
+// userName = '5';
+
+const isBirsday: boolean = true;
+const age: number = 40;
+const userName: string = 'John';
+
+if (isBirsday) {
+  console.log(`Congrats ${userName}, age: ${age + 1}`);
+}
