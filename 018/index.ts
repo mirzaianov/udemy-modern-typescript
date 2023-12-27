12bbb7afb89e1d211b7fabf31c83991f7e4e5f33
msejak <7o.mseJak@gmail.com>
2023-12-27 14:52:55 +0300

    update: lesson 018

---
+++ 018/index.ts
@@ -0,0 +1,10 @@
+const message: string | number = 5;
+
+const messages: string[] | number[] = ['a', 'b'];
+
+function printMsg(msg: string | number): void {
+  console.log(msg.toLowerCase());
+}
+
+printMsg(4);
+printMsg('msg');
