93f42572783477a465421fafa8aeb076abc28fac
msejak <7o.mseJak@gmail.com>
2024-01-12 19:14:35 +0300

    fix: lesson 043

--- 018/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const message = 5;
-const messages = ['a', 'b'];
-function printMsg(msg) {
-    console.log(msg.toLowerCase());
-}
-printMsg(4);
-printMsg('msg');
